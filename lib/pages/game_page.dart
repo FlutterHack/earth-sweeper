6e47fb137dd5a43b3eb117b00014e8d644dc1743
mert <utarit.543@gmail.com>
2020-06-28 23:46:10 +0300

    game provider

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,23 +1,21 @@
 import 'package:earthsweeper/models/game.dart';
+import 'package:earthsweeper/providers/game_settings_provider.dart';
 import 'package:earthsweeper/widgets/mine_sweeper.dart';
 import 'package:earthsweeper/widgets/windows95/src/scaffold95.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold95(
-          title: "Game",
-          body: Container(
-            child: MineSweeper(
-            gameModel: Game(
-              type: GameType.custom,
-              width: 10,
-              height: 10,
-              mineCount: 1,
-            ),
-        )),
-      ));
+      title: "Game",
+      body: Container(
+          child: MineSweeper(
+        gameModel:
+            Provider.of<GameSettingsProvider>(context, listen: false).settings,
+      )),
+    ));
   }
 }
